2c75e9212f06d8496553eb5026163ca21745c274
Gleb Bahmutov <gleb@kensho.com>
2014-06-09 23:06:38 -0400

    initial

---
+++ index.js
@@ -0,0 +1,38 @@
+var originalWrite = process.stdout.write.bind(process.stdout);
+
+var output = '';
+process.stdout.write = function () {
+  originalWrite('.');
+  // originalWrite.apply(1, arguments);
+  output += Array.prototype.join.call(arguments, '');
+};
+var interval;
+var originalExit = process.exit;
+function exit(exitCode) {
+  clearInterval(interval);
+  process.emit('timegruntexit', exitCode);
+}
+
+interval = setInterval(function () {
+  process.exit = exit;
+}, 1);
+process.exit = exit;
+
+process.on('SIGINT', function () {
+  process.exit();
+});
+
+process.once('timegruntexit', function (exitCode) {
+  clearInterval(interval);
+  process.exit = originalExit;
+
+  process.stdout.write = originalWrite;
+
+  if (exitCode) {
+    process.stdout.write('\n' + output);
+  } else {
+    process.stdout.write('\n');
+  }
+
+  process.exit(exitCode);
+});
